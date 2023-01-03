558432754b90f02d5dc1842ed9f104e93673a981
SamMarckson <115665088+SamMarckson@users.noreply.github.com>
2023-01-02 21:04:07 -0300

    Add files via upload
    Codewars Exercise

---
+++ Number of trailing zeros of N!.py
@@ -0,0 +1,33 @@
+"""
+Title: Number of trailing zeros of N!
+Source: https://www.codewars.com/kata/52f787eb172a8b4ae1000a34/train/python
+
+Write a program that will calculate the number of trailing zeros in a factorial of a given number.
+N! = 1 * 2 * 3 *  ... * N
+Be careful 1000! has 2568 digits...
+For more info, see: http://mathworld.wolfram.com/Factorial.html
+Examples:
+zeros(6) = 1
+# 6! = 1 * 2 * 3 * 4 * 5 * 6 = 720 --> 1 trailing zero
+zeros(12) = 2
+# 12! = 479001600 --> 2 trailing zeros
+Hint: You're not meant to calculate the factorial. Find another way to find the number of zeros.
+"""
+
+from math import log
+
+def zeros(n):
+
+    if n == 0:
+        return 0
+
+    soma = 0
+
+    K = int(log(n, 5))
+    #print(K)
+    for i in range(1, K+1):
+        soma += (n//(5**i))
+    return soma
+
+#MAIN
+print(zeros(913115578))
